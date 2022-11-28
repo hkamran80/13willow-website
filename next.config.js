999a8de6f90c3544ec3ea0dc5e1699dbb0993032
H. Kamran <hkamran@hkamran.com>
2022-11-27 20:10:11 -0800

    Add configurations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack: (config) => {
+        config.experiments = { topLevelAwait: true, layers: true };
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
